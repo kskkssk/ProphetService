4b8b74737431b1b2f1fedf8004fe9eee3032d564
Александра Кудашева <kudasheva0.kudasheva@gmail.com>
2024-07-03 03:03:35 +0300

    logic.py

---
+++ logic.py
@@ -0,0 +1,73 @@
+from typing import List, Dict
+import joblib
+import pandas as pd
+from datetime import datetime
+from prophet import Prophet
+
+
+class Request:
+    def __init__(self, data, model):
+        self.data = data
+        self.model = model
+
+    def validate(self):
+        try:
+            date = pd.to_datetime(self.data, format='%Y-%m-%d')
+            return date
+        except ValueError:
+            raise ValueError('Ожидается дата в формате YYYY-MM-DD')
+
+    def prediction(self):
+        date = self.validate()
+        future = pd.DataFrame({'ds': [date]})
+        prediction = self.model.predict(future)
+        return prediction, self.data
+
+
+class User:
+    def __init__(self, username: str, first_name: str, last_name: str, balance: float, password: str):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.balance = balance
+        self.transaction_list = {}
+
+    def check_balance(self) -> float:
+        return self.balance
+
+    def add_balance(self, amount: float):
+        self.balance += amount
+        return self.balance
+
+    def deduct_balance(self, amount: float) -> bool:
+        if amount <= self.balance:
+            self.balance -= amount
+            return True
+        return False
+
+    def transaction_history(self) -> List[Dict]:
+        return self.transaction_list
+
+    def handle_request(self, request: Request):
+        prediction, data = request.prediction()
+        count_sum = 0
+        if self.deduct_balance(10):
+            count_sum += 10
+            self.transaction_list['spent_money'] = count_sum
+            self.transaction_list['prediction'] = prediction.to_dict(orient='records')
+            self.transaction_list['data'] = data
+        return self.transaction_list
+
+
+m = joblib.load('/home/sasha/Загрузки/prophet_model.pkl')
+
+if __name__ == '__main__':
+    user = User('kk', 'Aleks', 'Kud', 10, '0880')
+    request = Request('2024-02-27', m)
+    try:
+        result = user.handle_request(request)
+    except:
+        raise ValueError("Ошибка")
+
+    print('История транзакций', user.transaction_history())
