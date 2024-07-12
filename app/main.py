34cde344c594d100677244b17f9afe5675460244
Denis Sergeich <denissergeich@Deniss-MacBook-Air.local>
2024-07-12 23:18:49 +0300

    change main.py

--- app/main.py
+++ app/main.py
@@ -9,38 +9,26 @@ if __name__ == '__main__':
         user_service = UserService(session)
         balance_service = BalanceService(session)
 
-        # Create admin user
         admin = user_service.create_user('admin', '0880', 'Admin', 'User', 'example@gmail.com', 100000000)
-        # Create demo user
         demo_user = user_service.create_user('demo', '0880', 'Demo', 'User', 'example@gmail.com', 20)
 
-        # No need to add users to session and commit here, if create_user handles it internally
-        # session.add(admin)
-        # session.add(demo_user)
-        # session.commit()
-
-        # Login as admin
         user_service.login('admin', '0880')
         if user_service.current_user:
             print("Logged in as:", user_service.current_user)
 
-            # Add balance to admin
             balance_service.add_balance(user_service.current_user.id, 20)
             print("Balance:", balance_service.get_balance(user_service.current_user.id))
 
-            # Load machine learning model
             model = joblib.load('prophet_model.pkl')
             request_data = '2024-10-10'
             request = Request(data=request_data, model=model)
 
-            # Handle request
             transaction = user_service.handle_request(request)
             if transaction:
                 print("Transaction:", transaction)
             else:
                 print("Transaction failed.")
 
-            # Print transaction history
             transaction_history = user_service.transaction_history()
             if transaction_history:
                 print("Transaction History:", transaction_history)
@@ -49,5 +37,4 @@ if __name__ == '__main__':
         else:
             print("Login failed.")
 
-    # session.close() is handled automatically by using the `with` statement
 
