3d031515ca0b72e49316d8ce7ee9da598177b5e1
Denis Sergeich <denissergeich@Deniss-MacBook-Air.local>
2024-07-23 22:19:25 +0300

    more beautiful architecture

--- app/routes/balance/get.py
+++ app/routes/balance/get.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
 from schemas.balance import BalanceResponse
+from fastapi import APIRouter, Depends
 from services.crud.personal_service import PersonService
 from services.crud.user_service import UserService
 from database.database import get_db
+from sqlalchemy.orm import Session
 
-balance_route = APIRouter(tags=['Balance'])
+
+balance_get_route = APIRouter(tags=['Balance'])
 
 
 def get_user_service(db: Session = Depends(get_db)) -> UserService:
@@ -17,22 +19,10 @@ def get_person_service(user_service: UserService = Depends(get_user_service)) ->
     return PersonService(user_service.session, current_user)
 
 
-@balance_route.post("/add_balance", response_model=BalanceResponse)
-async def add_balance(amount: float, person_service: PersonService = Depends(get_person_service)):
-    try:
-        new_balance = person_service.add_balance(amount)
-        return BalanceResponse(id=new_balance.user_id, amount=new_balance.amount)
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
-
-@balance_route.get("/balance", response_model=BalanceResponse)
+@balance_get_route.get("/get_balance", response_model=BalanceResponse)
 async def get_balance(person_service: PersonService = Depends(get_person_service)):
     try:
         balance = person_service.get_balance()
         return BalanceResponse(id=balance.user_id, amount=balance.amount)
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
-
-
-
---
+++ app/routes/user/post.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.orm import Session
+from services.crud.user_service import UserService
+from services.crud.personal_service import PersonService
+from database.database import get_db
+from schemas.user import UserCreate, UserResponse, UserSignin
+
+user_post_route = APIRouter(tags=['User'])
+
+
+def get_user_service(db: Session = Depends(get_db)) -> UserService:
+    return UserService(db)
+
+
+def get_person_service(user_service: UserService = Depends(get_user_service)) -> PersonService:
+    current_user = user_service.get_current_user()
+    return PersonService(user_service.session, current_user)
+
+
+@user_post_route.post("/signin", response_model=UserResponse)
+async def signin(data: UserCreate, user_service: UserService = Depends(get_user_service)):
+    if user_service.get_user_by_email(data.email) is not None:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User exists')
+    user = user_service.create_user(username=data.username, password=data.password, first_name=data.first_name, last_name=data.last_name, email=data.email)
+    return UserResponse.from_orm(user)
+
+
+@user_post_route.post("/signup", response_model=UserResponse)
+async def signup(data: UserSignin, user_service: UserService = Depends(get_user_service)):
+    if not user_service.login(username=data.username, password=data.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User does not exist')
+    return UserResponse.from_orm(user_service.get_current_user())
+
+
+@user_post_route.post("/handle_request", response_model=dict)
+async def handle_request(data: str, model, person_service: PersonService = Depends(get_person_service)):
+    try:
+        transaction_data = person_service.handle_request(data, model)
+        return transaction_data
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@user_post_route.post("/logout", response_model=dict)
+async def logout(user_service: UserService = Depends(get_user_service)):
+    try:
+        user_service.logout()
+        return {"message": "Вы успешно вышли из профиля"}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+
+
